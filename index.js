cf6f25abd9f4592c24c6a05d36a37b42a5128e0f
Volodymyr <datsik.vv@gmail.com>
2020-08-09 20:25:49 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,11 @@
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf("1332711625:AAGVh3akaKlM7qu06QTnE5ewGAetuJQLZzM")
+bot.start((ctx) => ctx.reply('go away'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply(''))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+bot.hears('good', (ctx) => ctx.reply('Im glad to hear this'))
+bot.hears('привет', (ctx) => ctx.reply('здарова'))
+bot.hears('Катя', (ctx) => ctx.reply('Самая красивая'))
