9b83845b0411ec37e5a430ce042e1dba123f86ed
Volodymyr <datsik.vv@gmail.com>
2020-08-10 11:10:46 +0300

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ bot.hears('good', (ctx) => ctx.reply('Im glad to hear this'));
 bot.hears('привет', (ctx) => ctx.reply('здарова'));
 bot.hears('Катя', (ctx) => ctx.reply('Самая красивая'));
 bot.hears('xx', (ctx) => ctx.reply('xxx'));
-bot.hears('Вова', (ctx) => ctx.reply('Пшел ты'));
+bot.hears('Вова', (ctx) => ctx.reply('Отличный парень'));
+bot.hears('вова', (ctx) => ctx.reply('Отличный парень'));
+bot.hears('Денис', (ctx) => ctx.reply('Петушок'));
+bot.hears('денис', (ctx) => ctx.reply('Петушок'));
+bot.hears('Ростик', (ctx) => ctx.reply('Петушок'));
+bot.hears('ростик', (ctx) => ctx.reply('Петушок'));
 
 bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 
